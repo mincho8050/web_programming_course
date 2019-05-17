e042904dc7ec51013edd340a439d14b4dae9b98e
Eunji Yu <49340180+mincho8050@users.noreply.github.com>
2019-05-17 21:37:36 +0900

    20190515_Day1

---
+++ Test01_Operator.java
@@ -0,0 +1,23 @@
+package oop0515;
+
+public class Test01_Operator {
+
+	public static void main(String[] args) {
+		
+		System.out.println(5+3);
+		System.out.println(5-3);
+		System.out.println(5*3);
+		System.out.println(5/3); //몫 연산자.
+		System.out.println(5/3.0); //실수형으로 변함. 소수값이 나옴.
+		System.out.println(5.0/3);
+		System.out.println(5./3);
+		System.out.println(5.0/3.0);
+		
+		// 정수형/정수형은 > 정수형
+		// 정수형/실수형 > 실수형
+		// 실수형/실수형 > 실수형
+
+
+	}
+
+}
